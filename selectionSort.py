fe5fed89b667b953f237cdf0b36fe8558a86ea24
Urchinzhou <urchinzhou@gmail.com>
2022-10-06 16:37:55 +0800

    add selectionSort

---
+++ selectionSort.py
@@ -0,0 +1,22 @@
+from cgitb import small
+
+
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest = findSmallest(arr)
+        newArr.append(arr.pop(smallest))
+    return newArr
+
+
+print(selectionSort([8, 30, 7, 4, 28, 43, 10]))
