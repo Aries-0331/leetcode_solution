728e087fe5871f3cfd0f4154e1df1379d3b6d6b7
Aries <urchinzhou@gmail.com>
2023-07-09 19:36:18 +0800

    update 125

---
+++ 125. Valid Palindrome.go
@@ -0,0 +1,56 @@
+```
+思路：
+先遍历一遍，去掉字母数字以外的字符，转小写，构建新的干净的字符串
+然后按照对称位置比对
+```
+func isPalindrome(s string) bool {
+    var sgood string
+    for i := 0; i < len(s); i++ {
+        if isalnum(s[i]) {
+            sgood += string(s[i])
+        }
+    }
+
+    n := len(sgood)
+    sgood = strings.ToLower(sgood)
+    for i := 0; i < n/2; i++ {
+        if sgood[i] != sgood[n-1-i] {
+            return false
+        }
+    }
+    return true
+}
+
+func isalnum(ch byte) bool {
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+}
+
+```
+思路2：
+先转小写，然后使用双指针，在转小写后的字符串上进行头尾比对，
+比对每个字符前先判断是否为有效字符，否则直接跳过，是则判断是否相等
+```
+func isPalindrome(s string) bool {
+    s = strings.ToLower(s)
+    left, right := 0, len(s) - 1
+    for left < right {
+        for left < right && !isalnum(s[left]) {
+            left++
+        }
+        for left < right && !isalnum(s[right]) {
+            right--
+        }
+        if left < right {
+            if s[left] != s[right] {
+                return false
+            }
+            left++
+            right--
+        }
+    }
+    return true
+}
+
+func isalnum(ch byte) bool {
+    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9')
+}
