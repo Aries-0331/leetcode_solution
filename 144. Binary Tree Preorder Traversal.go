8fe0a1fa957b8f37fdb149c41dc40973a4c6fca5
Aries <urchinzhou@gmail.com>
2023-07-15 21:25:53 +0800

    update 144

---
+++ 144. Binary Tree Preorder Traversal.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+ func preorderTraversal(root *TreeNode) (vals []int) {
+    var preorder func(*TreeNode)
+    preorder = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        vals = append(vals, node.Val)
+        preorder(node.Left)
+        preorder(node.Right)
+    }
+    preorder(root)
+    return
+}
