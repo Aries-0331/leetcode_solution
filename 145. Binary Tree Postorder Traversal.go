a2d41293311c148a9ea1e216fccb9ab03a551b45
Aries <urchinzhou@gmail.com>
2023-07-17 21:19:22 +0800

    update 145

---
+++ 145. Binary Tree Postorder Traversal.go
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+ func postorderTraversal(root *TreeNode) (res []int) {
+    var postorder func(*TreeNode)
+    postorder = func(node *TreeNode){
+        if node == nil {
+            return
+        }
+        postorder(node.Left)
+        postorder(node.Right)
+        res = append(res, node.Val)
+    }
+    postorder(root)
+    return
+}
