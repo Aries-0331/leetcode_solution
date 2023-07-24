c10f8092686a4bbbc4b73e118408cfca66d481ca
Aries <urchinzhou@gmail.com>
2023-07-24 18:11:54 +0800

    update 338

--- 338. Counting Bits.go
+++ 338. Counting Bits.go
@@ -13,3 +13,18 @@ func countBits(n int) []int {
     }
     return ret
 }
+
+/*
+动态规划
+*/
+func countBits(n int) []int {
+    bits := make([]int, n+1)
+    highBit := 0
+    for i := 1; i <= n; i++ {
+        if i&(i-1) == 0 {
+            highBit = i
+        }
+        bits[i] = bits[i-highBit] + 1
+    }
+    return bits
+}
