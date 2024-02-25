700b98cd6c843836fe727352aaee066f96229769
Aries <urchinzhou@gmail.com>
2024-02-25 23:16:44 +0800

    c++ exercise

---
+++ two sum.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <unordered_map> // Include the necessary header file
+
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::unordered_map<int, int> hashtable; 
+        for (int i = 0; i < nums.size(); i++){
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()){
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+    }
+};
